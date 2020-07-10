988af776233e8e74a5cbebee45204768a5abea0d
Vineet Singh <singhvineet45@gmail.com>
2020-07-10 17:11:36 +0530

    Initial Commit

---
+++ ELDMandateTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ELDMandateTask
+//
+//  Created by Vineet Singh on 10/07/20.
+//  Copyright © 2020 ELDMandate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
